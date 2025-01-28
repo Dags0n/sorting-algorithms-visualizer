75e0a4f5b9f46043901b4a5f1aaedb68f78ae1f5
Dagson Gabriel <dagson.g.01@gmail.com>
2025-01-28 15:14:40 -0300

    add main menu initialization

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import br.ufrn.imd.view.MainMenu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.setVisible(true);
     }
 }
